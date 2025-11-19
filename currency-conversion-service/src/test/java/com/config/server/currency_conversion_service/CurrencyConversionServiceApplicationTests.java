8aa9e3341ef806376119362fe1decdbc0d23b9e5
Dileep Vakkala <dileepvakkala@gmail.com>
2025-11-19 14:47:24 +0530

    #index for preparations

---
+++ currency-conversion-service/src/test/java/com/config/server/currency_conversion_service/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config.server.currency_conversion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
